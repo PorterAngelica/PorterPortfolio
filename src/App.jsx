2f0cd2f3035ce1541c0dc2a5d01b406641ccb642
Angelica Porter <angelicamelanytrejo12@gmail.com>
2024-04-26 11:41:29 -0400

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from "./pages/home/Home.jsx"
 import "./App.css"
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom"
